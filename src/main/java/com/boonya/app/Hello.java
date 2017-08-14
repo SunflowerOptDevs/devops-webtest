957ce0e60c3abcd970c025b6b92bc2ef356a8bf9
Administrator <Administrator@Pengjunlin>
2017-08-14 17:48:43 +0800

    Update Hello.java

--- src/main/java/com/boonya/app/Hello.java
+++ src/main/java/com/boonya/app/Hello.java
@@ -1,6 +1,9 @@
 package com.boonya.app;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -25,7 +28,9 @@ public class Hello extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		response.getWriter().write("<h3 style='color:red'>com.boonya.app.Hello extends HttpServlet</h3>");
+		SimpleDateFormat sdf=new SimpleDateFormat("yyy-mm-dd HH:mm:ss");
+		String div="<div style='color:red'><H1>Hello World<H1></div>";
+		response.getWriter().write("Hello: current time is "+sdf.format(new Date())+"\n"+div);
 	}
 
 	/**

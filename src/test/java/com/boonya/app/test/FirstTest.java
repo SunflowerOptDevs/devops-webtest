417d81f764f8a674658e4d20f373a0ee1349fcd1
Administrator <Administrator@Pengjunlin>
2017-08-18 14:31:47 +0800

    Update First.java

--- src/test/java/com/boonya/app/test/FirstTest.java
+++ src/test/java/com/boonya/app/test/FirstTest.java
@@ -2,45 +2,86 @@ package com.boonya.app.test;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
+
+import java.util.Random;
+
 import org.testng.annotations.AfterClass;
 
 public class FirstTest {
 	@Test
 	public void a() {
+		try {
+			Thread.sleep(new Random(1000).nextLong());
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println("FirstTest a method....sucess!");
 	}
 
 	@Test
 	public void b() {
+		try {
+			Thread.sleep(new Random(1000).nextLong());
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println("FirstTest b method....sucess!");
 	}
 
 	@Test
 	public void c() {
+		try {
+			Thread.sleep(new Random(1000).nextLong());
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println("FirstTest c method....sucess!");
 	}
 
 	@Test
 	public void d() {
+		try {
+			Thread.sleep(new Random(1000).nextLong());
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println("FirstTest d method....sucess!");
 	}
 
 	@Test
 	public void e() {
+		try {
+			Thread.sleep(new Random(1000).nextLong());
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println("FirstTest e method....sucess!");
 	}
 
 	@Test
 	public void f() {
+		try {
+			Thread.sleep(new Random(1000).nextLong());
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println("FirstTest f method....sucess!");
 	}
 
 	@BeforeClass
 	public void beforeClass() {
+		System.out.println(".......................beforeClass");
 	}
 
 	@AfterClass
 	public void afterClass() {
+		System.out.println(".......................afterClass");
 	}
 
 }

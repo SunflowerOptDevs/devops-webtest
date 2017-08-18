4ba4b112fcb49b31de5b9db227fbfb13e0944586
boonyachengdu <boonya@sina.com>
2017-08-18 18:46:14 +0800

    Selenium no test.

--- src/test/java/com/boonya/app/test/SeleniumTest.java
+++ src/test/java/com/boonya/app/test/SeleniumTest.java
@@ -1,8 +1,10 @@
 package com.boonya.app.test;
 
+import org.junit.runners.Parameterized.Parameters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterClass;
@@ -11,33 +13,51 @@ import org.testng.annotations.Test;
 
 public class SeleniumTest {
 	
+	@Parameters(name="driverPath")
 	@Test
-	public void c() {
+	public void b(String driverPath) {
 		WebDriver driver;
+		System.setProperty("webdriver.ie.driver", driverPath); 
+		driver = new InternetExplorerDriver();
+		driver.get("http://www.baidu.com");
+		System.out.println(driver.getCurrentUrl());
+	}
+	
+	@Parameters(name="driverPath")
+	@Test
+	public void c(String driverPath) {
+		WebDriver driver;
+		System.setProperty("webdriver.safari.bin", driverPath);
 		driver = new SafariDriver();
 		driver.get("http://www.baidu.com");
 		System.out.println(driver.getCurrentUrl());
 	}
 	
+	@Parameters(name="driverPath")
 	@Test
-	public void d() {
+	public void d(String driverPath) {
 		WebDriver driver;
+		System.setProperty("webdriver.opera.bin", driverPath);
 		driver = new OperaDriver();
 		driver.get("http://www.baidu.com");
 		System.out.println(driver.getCurrentUrl());
 	}
 	
+	@Parameters(name="driverPath")
 	@Test
-	public void e() {
+	public void e(String driverPath) {
 		WebDriver driver;
+		System.setProperty("webdriver.chrome.bin", driverPath);
 		driver = new ChromeDriver();
 		driver.get("http://www.baidu.com");
 		System.out.println(driver.getCurrentUrl());
 	}
 
+	@Parameters(name="driverPath")
 	@Test
-	public void f() {
+	public void f(String driverPath) {
 		WebDriver driver;
+		System.setProperty("webdriver.firefox.bin", driverPath);
 		driver = new FirefoxDriver();
 		driver.get("http://www.baidu.com");
 		System.out.println(driver.getCurrentUrl());

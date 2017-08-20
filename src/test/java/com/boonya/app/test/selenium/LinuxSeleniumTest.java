b6c0218f7f51732f49b6555ac2258a6bec3e052a
boonyachengdu <boonya@sina.com>
2017-08-20 20:19:13 +0800

    Update LinuxSeleniumTest.java

--- src/test/java/com/boonya/app/test/selenium/LinuxSeleniumTest.java
+++ src/test/java/com/boonya/app/test/selenium/LinuxSeleniumTest.java
@@ -23,10 +23,7 @@ public class LinuxSeleniumTest {
 	@Test
 	public void firefox() {
 		WebDriver driver;
-		// Install firefox in centos: yum install -y firefox
-		System.setProperty("webdriver.firefox.bin", "/usr/bin/firefox");
-		// Incorrect way
-		//System.setProperty("webdriver.gecko.driver", "geckodriver/linux/geckodriver");
+		System.setProperty("webdriver.gecko.driver", "geckodriver/linux/geckodriver");
 		driver = new FirefoxDriver();
 		driver.get("http://www.baidu.com");
 		System.out.println(driver.getCurrentUrl());
